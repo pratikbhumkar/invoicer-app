98a3d36b64af918b260f80cacb9b72e7c883ccce
pratikbhumkar <prateekbhumkar@gmail.com>
2022-10-14 13:07:31 +1100

    fix failing test

--- src/components/PageHead.test.tsx
+++ src/components/PageHead.test.tsx
@@ -1,15 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import { PageHead } from './PageHead'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 describe('header', () => {
   test('should render the Page Header correctly', async () => {
     await render(
-      <PageHead
-        pageHeaderText="Customer"
-        pageSubHeaderText="Manage all your Customers"
-        activeTab=""
-        baseLocation="customer"
-      />
+      <Router>
+        <PageHead
+          pageHeaderText="Customer"
+          pageSubHeaderText="Manage all your Customers"
+          activeTab=""
+          baseLocation="customer"
+        />
+      </Router>
     )
     expect(await screen.findByText('Manage all your Customers')).toBeVisible()
     expect(await screen.findByText('Customer')).toBeVisible()
